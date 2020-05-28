2293915e7f0b74c3f950285a032d84b91c45a8ff
tkm <sumikawa.web@gmail.com>
2020-05-28 14:41:15 +0900

    add npm scripts,babel and image-loader

--- webpack.config.js
+++ webpack.config.js
@@ -4,71 +4,99 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: './src/javascripts/main.js',
-    output: {
-        path: path.resolve(__dirname, './dist'),
-        filename: './javascripts/main.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(css|sass|scss)/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                    },
-                    {
-                        loader: 'css-loader',
-                    },
-                    {
-                        loader: 'sass-loader',
-                    }
-                ]
-            },
-            {
-                test: /\.(png|jpg)/,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            esModule: false,
-                            name: 'images/[name].[ext]',
-                        }
-                    }
-                ]
-            },
-            {
-                test: /\.pug/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                    },
-                    {
-                        loader: 'pug-html-loader',
-                        options: {
-                            pretty: true,
-                        }
-                    }
-                ]
+  mode: 'development',
+  devtool: 'source-map',
+  entry: './src/javascripts/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: './javascripts/main.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                ['@babel/preset-env', {'targets': '> 30%, not dead'}],
+              ]
             }
+          }
         ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: './stylesheets/main.css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/index.pug',
-            filename: 'index.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/access.pug',
-            filename: 'access.html'
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/templates/members/taro.pug',
-            filename: 'members/taro.html'
-        }),
-        new CleanWebpackPlugin(),
+      },
+      {
+        test: /\.(css|sass|scss)/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: false,
+            }
+          },
+          {
+            loader: 'sass-loader',
+          }
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg)/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              esModule: false,
+              name: 'images/[name].[ext]',
+            }
+          },
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+                quality: 65,
+              }
+            }
+          }
+        ]
+      },
+      {
+        test: /\.pug/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+          {
+            loader: 'pug-html-loader',
+            options: {
+              pretty: true,
+            }
+          }
+        ]
+      }
     ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: './stylesheets/main.css',
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/index.pug',
+      filename: 'index.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/access.pug',
+      filename: 'access.html'
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/templates/members/taro.pug',
+      filename: 'members/taro.html'
+    }),
+    new CleanWebpackPlugin(),
+  ]
 }
